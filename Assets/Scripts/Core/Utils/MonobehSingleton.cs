9a7441f4da553480ab3c86ac4ca48af79db9dffd
Alexey_Afonin <45484621+AlexeyAfonin@users.noreply.github.com>
2023-04-29 01:51:29 +0300

    Add util MohobehSingleton
    Implements the "Singleton" pattern for the "Monobehavior" classes

---
+++ Assets/Scripts/Core/Utils/MonobehSingleton.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MyCat.Core.Util
+{
+    [DisallowMultipleComponent]
+    public class MonobehSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        [SerializeField] protected bool isDontDestroy;
+
+        protected static T Reference;
+
+        /// <summary>
+        /// The singleton instance of a reference.
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (Reference == null)
+                {
+                    if (!(Reference = FindObjectOfType<T>()))
+                    {
+                        throw new MissingReferenceException($"The singleton reference to a {typeof(T).Name} does not found!");
+                    }
+                }
+
+                return Reference;
+            }
+        }
+
+        public static bool HasReference
+        {
+            get
+            {
+                if (Reference == null)
+                {
+                    return (Reference = FindObjectOfType<T>()) != null;
+                }
+
+                return true;
+            }
+        }
+
+        protected void Awake()
+        {
+            if (isDontDestroy)
+            {
+                DontDestroyOnLoad(Instance);
+            }
+        }
+
+        protected void Reset()
+        {
+#if UNITY_EDITOR
+            if (FindObjectsOfType<T>().Length > 1)
+            {
+                EditorUtility.DisplayDialog("Singleton Error", $"There should never be more than 1 reference of {typeof(T).Name}!", "OK");
+                DestroyImmediate(this);
+            }
+#endif
+        }
+    }
+}
