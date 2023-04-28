306543b64318d10464091f218f4b8be7359c0458
Alexey_Afonin <45484621+AlexeyAfonin@users.noreply.github.com>
2023-04-29 01:53:17 +0300

    Add class Enums and add enum "Mood"

---
+++ Assets/Scripts/Core/Utils/Enums.cs
@@ -0,0 +1,13 @@
+namespace MyCat.Core.Util
+{
+    public static class Enums
+    {
+        public enum Mood
+        {
+            None = 0,
+            Bad = 1,
+            Good = 2,
+            Great = 3,
+        }
+    }
+}
